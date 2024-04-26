a362e350cf9c081496db5bd52e08efb5495d0dc8
Ivan Poluhin <ivan.poluhin.200407@gmail.com>
2024-04-26 21:39:24 +0300

    added sideNav, slideToggle

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatListModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
